1f4eb62e70cdbb4c4c0be566cb0b0d8346e34ec0
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-19 16:41:05 +0800

    Fix source maps

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -3,7 +3,6 @@ import { Plugin } from 'rollup';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import * as babel from '@babel/core';
 import path from 'path';
-import ts from '@babel/preset-typescript';
 
 export interface SilmarilPluginFilter {
   include?: FilterPattern;
@@ -26,22 +25,36 @@ export default function silmarilPlugin(
     name: 'silmaril',
     async transform(code, id) {
       if (filter(id)) {
+        
+        const pluginOption = [silmarilBabel, {}];
+        const plugins: NonNullable<NonNullable<babel.TransformOptions['parserOpts']>['plugins']> = ['jsx'];
+        if (/\.[mc]?tsx?$/i.test(id)) {
+          plugins.push('typescript');
+        }
         const result = await babel.transformAsync(code, {
           ...options.babel,
-          presets: [
-            [ts],
-            ...(options.babel?.presets ?? []),
-          ],
           plugins: [
-            [silmarilBabel],
-            ...(options.babel?.plugins ?? []),
+            pluginOption,
+            ...(options.babel?.plugins || []),
           ],
+          parserOpts: {
+            ...(options.babel?.parserOpts || {}),
+            plugins: [
+              ...(options.babel?.parserOpts?.plugins || []),
+              ...plugins,
+            ],
+          },
           filename: path.basename(id),
+          ast: false,
+          sourceMaps: true,
+          configFile: false,
+          babelrc: false,
+          sourceFileName: id,
         });
 
         if (result) {
           return {
-            code: result.code ?? '',
+            code: result.code || '',
             map: result.map,
           };
         }
